aa18875547df2cfc289d8653bad6b99626c20386
pavel <pavelpp1974@gmail.com>
2024-07-15 21:17:08 +0300

    init remix spa

---
+++ app/routes/_index.tsx
@@ -0,0 +1,28 @@
+import type { MetaFunction } from "@remix-run/node";
+import {
+  Link,
+} from "@remix-run/react";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix SPA" },
+    { name: "description", content: "Welcome to Remix (SPA Mode)!" },
+  ];
+};
+
+export default function Index() {
+  return (
+        <div id="sidebar">
+          <nav>
+            <ul>
+              <li>
+                <Link to={`/contacts/1`}>Your Name</Link>
+              </li>
+              <li>
+                <Link to={`/contacts/2`}>Your Friend</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+  );
+}
